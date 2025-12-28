f7bb1677e6696c4b7dfb8708e8b7d4725ac0373f
kosero <kosero@tuta.io>
2025-12-28 22:55:55 +0300

    feat: simple while loop

--- src/main.c
+++ src/main.c
@@ -1,1 +1,22 @@
-int main(void) { return 0; }
+#include "linenoise.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PROMPT "$ "
+#define HISTORY_LENGTH 1024
+
+int main(void) {
+  if (!linenoiseHistorySetMaxLen(HISTORY_LENGTH)) {
+    fprintf(stderr, "Could not set history!");
+    exit(1);
+  }
+
+  char *line;
+  while ((line = linenoise(PROMPT)) != NULL) {
+    fprintf(stdout, "%s\n", line);
+    linenoiseHistoryAdd(line);
+    linenoiseFree(line);
+  }
+
+  return 0;
+}
